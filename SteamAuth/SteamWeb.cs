cc6f1175ec5d6034a53f3786836a4c6186088fa9
NiceAesth <nice@aesth.dev>
2024-10-11 19:13:27 +0300

    fix: allow nullable cookies and body in SteamWeb

--- SteamAuth/SteamWeb.cs
+++ SteamAuth/SteamWeb.cs
@@ -9,22 +9,24 @@ namespace SmogAuthCore;
 
 public static class SteamWeb
 {
-    public static readonly string MobileAppUserAgent = "Dalvik/2.1.0 (Linux; U; Android 9; Valve Steam App Version/3)";
+    public const string MobileAppUserAgent = "Dalvik/2.1.0 (Linux; U; Android 9; Valve Steam App Version/3)";
 
-    public static async Task<string> GetRequest(string url, CookieContainer cookies)
+    public static async Task<string> GetRequest(string url, CookieContainer? cookies)
     {
-        using var handler = new HttpClientHandler { CookieContainer = cookies };
+        using var handler = new HttpClientHandler();
+        if (cookies != null) handler.CookieContainer = cookies;
         using var client = new HttpClient(handler);
         client.DefaultRequestHeaders.UserAgent.ParseAdd(MobileAppUserAgent);
         var response = await client.GetStringAsync(url);
         return response;
     }
 
-    public static async Task<string> PostRequest(string url, CookieContainer cookies, NameValueCollection body)
+    public static async Task<string> PostRequest(string url, CookieContainer? cookies, NameValueCollection? body)
     {
         body ??= new NameValueCollection();
 
-        using var handler = new HttpClientHandler { CookieContainer = cookies };
+        using var handler = new HttpClientHandler();
+        if (cookies != null) handler.CookieContainer = cookies;
         using var client = new HttpClient(handler);
         client.DefaultRequestHeaders.UserAgent.ParseAdd(MobileAppUserAgent);
         var content = new FormUrlEncodedContent(body.Cast<string>().ToDictionary(k => k, k => body[k]));
